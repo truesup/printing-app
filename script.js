9965e1f8e0ff25f932cc45e3e65e02443a2cd725
Pavel Merekin <merekin33@gmail.com>
2025-01-13 22:12:26 +0700

    paragraphs.js is ready and minor changes applied

---
+++ script.js
@@ -0,0 +1,23 @@
+import { paragraphs } from './paragraphs.js';
+
+const windowWidth = window.innerWidth;
+const wrapperOuter = document.querySelector('.wrapper-outer');
+
+// if (windowWidth < 1024) {
+//   wrapperOuter.classList.add('visually-hidden');
+//   const div = document.createElement('div');
+//   div.textContent = "App doesn't work on small devices";
+//   div.style.color = '#f1f1f1';
+//   div.style.fontSize = '20px';
+//   document.body.appendChild(div);
+// } else {
+//   wrapperOuter.classList.remove('visually-hidden');
+// }
+
+const typingText = document.querySelector('.paragraph');
+
+function getRandomText() {
+  console.log(paragraphs[0]);
+}
+
+getRandomText();
