26e4893ca9d87ef5704728202644a0afa0f66d78
Pavel Merekin <merekin33@gmail.com>
2025-01-13 23:04:16 +0700

    typing feature added, backspace event added

--- script.js
+++ script.js
@@ -14,10 +14,47 @@ const wrapperOuter = document.querySelector('.wrapper-outer');
 //   wrapperOuter.classList.remove('visually-hidden');
 // }
 
-const typingText = document.querySelector('.paragraph');
+const typingText = document.querySelector('.paragraph p');
+const input = document.querySelector('input');
+
+let charIndex = 0;
 
 function getRandomText() {
-  console.log(paragraphs[0]);
+  const randomIndex = Math.floor(Math.random() * paragraphs.length);
+  paragraphs[randomIndex].split('').forEach(span => {
+    const spanTag = `<span>${span}</span>`;
+    typingText.innerHTML += spanTag;
+  });
+
+  document.addEventListener('keydown', () => {
+    input.focus();
+  });
+  typingText.addEventListener('click', () => {
+    input.focus();
+  });
+}
+
+function initializeTyping() {
+  const characters = typingText.querySelectorAll('span');
+  let typedChar = input.value.split('')[charIndex];
+
+  if (typedChar == null) {
+    charIndex--;
+    characters[charIndex].classList.remove('correct', 'incorrect');
+  } else {
+    if (characters[charIndex].innerText === typedChar) {
+      characters[charIndex].classList.add('correct');
+    } else {
+      characters[charIndex].classList.add('incorrect');
+    }
+
+    charIndex++;
+  }
+
+  characters.forEach(span => span.classList.remove('active'));
+  characters[charIndex].classList.add('active');
 }
 
 getRandomText();
+
+input.addEventListener('input', initializeTyping);
